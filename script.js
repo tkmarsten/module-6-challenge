ae9fd58d463d645de45db9342c0b3eb7228b4171
tkmarsten <tkmarsten@gmail.com>
2023-03-04 00:08:14 -0800

    Hide elements until needed

--- script.js
+++ script.js
@@ -30,71 +30,14 @@ function updateWeather(data) {
   description.innerHTML = data.weather[0].description
   humidity.innerHTML = data.main.humidity + '%'
   wind.innerHTML = data.wind.speed + 'Km/h'
+
+  const weather = document.querySelector('.weather')
+  weather.style.display = 'block'
+
+  const details = document.querySelector('.details')
+  details.style.display = 'flex'
 }
 
-
-
-/**
- * Helper function that accepts a forecast object to create the individual cards.
- * @param {Object} forecast 
- * @returns HTML div
-function createCard(forecast) {
-  const divEl = document.createElement('div')
-  divEl.classList.add('forecast-card')
-
-  const dateEl = document.createElement('div')
-  dateEl.textContent = forecast.date
-  divEl.append(dateEl)
-
-  const tempEl = document.createElement('div')
-  tempEl.innerHTML = forecast.temp + '&deg'
-  divEl.append(tempEl)
-
-  const iconEl = document.createElement('img')
-  iconEl.setAttribute('src', 'https://openweathermap.org/img/wn/' + forecast.icon + '@2x.png')
-  divEl.append(iconEl)
-
-  const descriptionEl = document.createElement('div')
-  descriptionEl.textContent = forecast.description
-  divEl.append(descriptionEl)
-
-  const humidityEl = document.createElement('div')
-  humidityEl.textContent = 'Humidity: ' + forecast.humidity + '%'
-  divEl.append(humidityEl)
-
-  const windEl = document.createElement('div')
-  let unitText = units === '&units=imperial' ? ' MPH' : ' KPH'
-  windEl.textContent = 'Wind speed: ' + forecast.wind + unitText
-  divEl.append(windEl)
-
-  return divEl
-}
-
-/**
- * Gets the current forecast of the city parameter.
- * @param {String} city 
-function getCurrentForecast(city) {
-  fetch(baseUrl + currentUrl + apiKey + units + '&q=' + city)
-    .then(function (res) {
-      return res.json()
-    })
-    .then(function (data) {
-      const currEl = document.querySelector('#current-forecast')
-
-      const dataObj = {
-        date: 'Today',
-        temp: data.main.temp,
-        icon: data.weather[0].icon,
-        description: data.weather[0].description,
-        humidity: data.main.humidity,
-        wind: data.wind.speed
-      }
-
-      const div = createCard(dataObj)
-
-      currEl.append(div)
-    })
-}
 
 /**
  * Gets the weekly forecast of the city parameter.
@@ -122,17 +65,4 @@ function getWeeklyForecast(city) {
       }
     })
 }
-
-/**
- * Accepts a city parameter which retrieves the forecast of that city.
- * @param {String} city
-function getForecast(city) {
-  document.querySelector('#current-forecast').textContent = ''
-  document.querySelector('#weekly-forecast').textContent = ''
-
-  document.querySelector('#forecastCity').textContent = city
-  getCurrentForecast(city)
-  getWeeklyForecast(city)
-}
-
 */
